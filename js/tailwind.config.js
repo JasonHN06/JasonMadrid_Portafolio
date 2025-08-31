ed51bce0e84c6b03fe043a090a593acc1a024d5c
Jason Madrid <jasonhnn6@gmail.com>
2025-08-30 21:36:35 -0600

    Test commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                "color-backone": "#080c1a",
+                "color-primary": "#ffffff",
+                "color-btn": "#3d2e51",
+                "color-hover": "#26223c",
+                "color-border": "#161829",
+                "color-backtwo": "#5b3b68",
+            },
+        },
+    },
+};
