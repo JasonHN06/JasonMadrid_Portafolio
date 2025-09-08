c177eb8f55f80c8c5e9172fd452d975d5cd0f316
Jason Madrid <jasonhnn6@gmail.com>
2025-09-08 17:06:06 -0600

    editando las cards de la seccion services

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -1,14 +1,23 @@
 tailwind.config = {
-    theme: {
-        extend: {
-            colors: {
-                "color-backone": "#080c1a",
-                "color-primary": "#ffffff",
-                "color-btn": "#3d2e51",
-                "color-hover": "#26223c",
-                "color-border": "#161829",
-                "color-backtwo": "#5b3b68",
-            },
+  theme: {
+    extend: {
+      colors: {
+        "color-backone": "#080c1a",
+        "color-primary": "#ffffff",
+        "color-btn": "#3d2e51",
+        "color-hover": "#26223c",
+        "color-border": "#161829",
+        "color-backtwo": "#5b3b68",
+      },
+      keyframes: {
+        fadeInUp: {
+          "0%": { opacity: "0", transform: "translateY(40px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
         },
+      },
+      animation: {
+        fadeInUp: "fadeInUp 0.8s ease-out forwards",
+      },
     },
+  },
 };
