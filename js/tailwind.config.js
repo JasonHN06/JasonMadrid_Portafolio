a09cc35d9b6e7f4daa7e556d6c2f009f6fc8985a
Jason Madrid <jasonhnn6@gmail.com>
2025-09-11 17:59:57 -0600

    edicion de textos y btns, queda por definir la fuente de las cards services

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -4,8 +4,8 @@ tailwind.config = {
       colors: {
         "color-backone": "#080c1a",
         "color-primary": "#ffffff",
-        "color-btn": "#3d2e51",
-        "color-hover": "#26223c",
+        "color-btn": "#f5f5f5",
+        "color-hover": "#86D2D5",
         "color-border": "#161829",
         "color-backtwo": "#5b3b68",
       },
@@ -20,6 +20,9 @@ tailwind.config = {
       },
       animation: {
         jiggle: "jiggle 0.4s ease-in-out",
+      },
+      fontFamily: {
+        courier: ["Courier New", "Courier", "monospace"],
       },
     },
   },
